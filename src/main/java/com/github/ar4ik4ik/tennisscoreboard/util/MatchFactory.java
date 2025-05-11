fc7337fafcf5543cb0fa3d5d1777e073f60cd02f
Arturo <arthursolokhin27@gmail.com>
2025-05-11 04:49:46 +0300

    fixed MatchFactory.java

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/util/MatchFactory.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/util/MatchFactory.java
@@ -5,6 +5,11 @@ import com.github.ar4ik4ik.tennisscoreboard.domain.Player;
 import com.github.ar4ik4ik.tennisscoreboard.rule.config.concreterules.ClassicGameRules;
 import com.github.ar4ik4ik.tennisscoreboard.rule.config.concreterules.ClassicMatchRules;
 import com.github.ar4ik4ik.tennisscoreboard.rule.config.concreterules.ClassicSetRules;
+import com.github.ar4ik4ik.tennisscoreboard.rule.config.concreterules.ClassicTieBreakRules;
+import com.github.ar4ik4ik.tennisscoreboard.rule.strategy.ClassicGameScoreStrategy;
+import com.github.ar4ik4ik.tennisscoreboard.rule.strategy.ClassicSetScoringStrategy;
+import com.github.ar4ik4ik.tennisscoreboard.rule.strategy.MatchScoringStrategy;
+import com.github.ar4ik4ik.tennisscoreboard.rule.strategy.TieBreakScoringStrategy;
 
 public class MatchFactory {
 
@@ -12,12 +17,22 @@ public class MatchFactory {
         var gameRule = new ClassicGameRules(4, 3, 1);
         var setRule = new ClassicSetRules(6, 2, true);
         var matchRule = new ClassicMatchRules(2);
+        var tieBreakRule = new ClassicTieBreakRules(2, 7);
+        var setStrategy = new ClassicSetScoringStrategy(setRule);
+        var gameStrategy = new ClassicGameScoreStrategy(gameRule);
+        var tieBreakStrategy = new TieBreakScoringStrategy(tieBreakRule);
+        var matchStrategy = new MatchScoringStrategy(matchRule);
+
         return Match.<Player>builder()
                 .firstCompetitor(firstPlayer)
                 .secondCompetitor(secondPlayer)
                 .matchRule(matchRule)
                 .setRule(setRule)
                 .gameRule(gameRule)
+                .strategy(matchStrategy)
+                .tieBreakStrategy(tieBreakStrategy)
+                .gameStrategy(gameStrategy)
+                .setStrategy(setStrategy)
                 .build();
     }
 

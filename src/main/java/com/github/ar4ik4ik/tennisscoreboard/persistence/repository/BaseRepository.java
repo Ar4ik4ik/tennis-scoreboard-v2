b81689b541e0424faa0247d632d55259ee74328c
Arturo <arthursolokhin27@gmail.com>
2025-05-13 01:52:13 +0300

    added logging

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/persistence/repository/BaseRepository.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/persistence/repository/BaseRepository.java
@@ -3,11 +3,13 @@ package com.github.ar4ik4ik.tennisscoreboard.persistence.repository;
 import com.github.ar4ik4ik.tennisscoreboard.persistence.entity.BaseEntity;
 import com.github.ar4ik4ik.tennisscoreboard.util.SessionManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
+@Log4j2
 @RequiredArgsConstructor
 public class BaseRepository<K extends Serializable, E extends BaseEntity<K>>
         implements Repository<K, E> {
@@ -19,6 +21,7 @@ public class BaseRepository<K extends Serializable, E extends BaseEntity<K>>
     public E save(E entity) {
         var entityManager = SessionManager.getSession();
         entityManager.persist(entity);
+        log.debug("Saved new entity {}", entity);
         return entity;
     }
 

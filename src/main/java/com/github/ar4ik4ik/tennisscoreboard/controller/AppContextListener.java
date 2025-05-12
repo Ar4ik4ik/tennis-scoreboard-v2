0edf285ec1f268930348a25d889cba1d09761c41
Arturo <arthursolokhin27@gmail.com>
2025-05-13 01:49:14 +0300

    added AppContextListener.java for app init

---
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/AppContextListener.java
@@ -0,0 +1,34 @@
+package com.github.ar4ik4ik.tennisscoreboard.controller;
+
+import com.github.ar4ik4ik.tennisscoreboard.persistence.repository.MatchRepository;
+import com.github.ar4ik4ik.tennisscoreboard.persistence.repository.PlayerRepository;
+import com.github.ar4ik4ik.tennisscoreboard.service.FinishedMatchesPersistenceService;
+import com.github.ar4ik4ik.tennisscoreboard.service.MatchScoreCalculationService;
+import com.github.ar4ik4ik.tennisscoreboard.service.OngoingMatchesService;
+import com.github.ar4ik4ik.tennisscoreboard.service.PlayerManagerService;
+import com.github.ar4ik4ik.tennisscoreboard.util.MatchFactory;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        var playerRepo = new PlayerRepository();
+        var matchRepo = new MatchRepository();
+
+        var playerService = new PlayerManagerService(playerRepo);
+        var ongoingMatchesService = new OngoingMatchesService(playerService, new MatchFactory());
+        var finishedMatchesService = new FinishedMatchesPersistenceService(matchRepo);
+        var matchScoreService = new MatchScoreCalculationService(ongoingMatchesService, finishedMatchesService);
+
+        sce.getServletContext().setAttribute("playerService", playerService);
+        sce.getServletContext().setAttribute("ongoingMatchesService", ongoingMatchesService);
+        sce.getServletContext().setAttribute("finishedMatchesService", finishedMatchesService);
+        sce.getServletContext().setAttribute("matchScoreService", matchScoreService);
+
+    }
+}

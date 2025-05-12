e821d8f91590febec04f1def74c87a69f044b48d
Arturo <arthursolokhin27@gmail.com>
2025-05-13 01:53:09 +0300

    added servlets

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/MatchScoreServlet.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/MatchScoreServlet.java
@@ -3,7 +3,6 @@ package com.github.ar4ik4ik.tennisscoreboard.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ar4ik4ik.tennisscoreboard.model.dto.ScoreIncreaseDto;
 import com.github.ar4ik4ik.tennisscoreboard.service.MatchScoreCalculationService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,14 +10,20 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/match-score")
+@WebServlet(urlPatterns = "/match-score/")
 public class MatchScoreServlet extends HttpServlet {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private final MatchScoreCalculationService matchScoreCalculationService = MatchScoreCalculationService.builder().build();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private MatchScoreCalculationService matchScoreCalculationService;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() {
+        matchScoreCalculationService = (MatchScoreCalculationService) getServletContext()
+                .getAttribute("matchScoreService");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String matchId = req.getParameter("uuid");
         String playerId = req.getParameter("playerId");
 
---
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/NewMatchServlet.java
@@ -0,0 +1,54 @@
+package com.github.ar4ik4ik.tennisscoreboard.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.ar4ik4ik.tennisscoreboard.model.dto.MatchRequestDto;
+import com.github.ar4ik4ik.tennisscoreboard.model.dto.PlayerRequestDto;
+import com.github.ar4ik4ik.tennisscoreboard.service.OngoingMatchesService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/new-match")
+public class NewMatchServlet extends HttpServlet {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private OngoingMatchesService ongoingMatchesService;
+
+    @Override
+    public void init() {
+        ongoingMatchesService = (OngoingMatchesService) getServletContext()
+                .getAttribute("ongoingMatchesService");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        String firstPlayer = req.getParameter("firstPlayer");
+        String secondPlayer = req.getParameter("secondPlayer");
+
+        var match = ongoingMatchesService.createNewMatch(MatchRequestDto.builder()
+                .firstPlayer(PlayerRequestDto.builder()
+                        .name(firstPlayer)
+                        .build())
+                .secondPlayer(PlayerRequestDto.builder()
+                        .name(secondPlayer)
+                        .build())
+                .build());
+
+        resp.getWriter().write(objectMapper.writeValueAsString(match));
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        var matchUUID = req.getParameter("uuid");
+        var currentMatch = ongoingMatchesService.getMatch(matchUUID);
+        resp.getWriter().write(objectMapper.writeValueAsString(currentMatch));
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}

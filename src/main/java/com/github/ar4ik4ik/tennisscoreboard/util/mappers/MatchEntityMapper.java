9fcf732b7fd9f23f4a62cf5c3d8abfd561500077
Arturo <arthursolokhin27@gmail.com>
2025-05-29 02:00:37 +0300

    removed unused method added method for finished matches page

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/util/mappers/MatchEntityMapper.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/util/mappers/MatchEntityMapper.java
@@ -2,6 +2,7 @@ package com.github.ar4ik4ik.tennisscoreboard.util.mappers;
 
 import com.github.ar4ik4ik.tennisscoreboard.domain.Match;
 import com.github.ar4ik4ik.tennisscoreboard.domain.Player;
+import com.github.ar4ik4ik.tennisscoreboard.model.dto.FinishedMatchResponseDto;
 import com.github.ar4ik4ik.tennisscoreboard.persistence.entity.MatchEntity;
 
 public class MatchEntityMapper {
@@ -14,14 +15,11 @@ public class MatchEntityMapper {
                 .build();
     }
 
-    public static Match<Player> fromEntity(MatchEntity match) {
-        var buildedMatch = Match.<Player>builder()
-                .firstCompetitor(PlayerEntityMapper.fromEntity(match.getFirstPlayer()))
-                .secondCompetitor(PlayerEntityMapper.fromEntity(match.getSecondPlayer()))
+    public static FinishedMatchResponseDto fromEntity(MatchEntity match) {
+        return FinishedMatchResponseDto.builder()
+                .firstPlayerName(match.getFirstPlayer().getName())
+                .secondPlayerName(match.getSecondPlayer().getName())
+                .winnerName(match.getWinner().getName())
                 .build();
-        // TODO Костыль с проставлением виннера, также нужно подумать насчет поля id,
-        //  остальные параметры типа правил кажутся не особо важными
-        buildedMatch.finishCompetition(PlayerEntityMapper.fromEntity(match.getWinner()));
-        return buildedMatch;
     }
 }

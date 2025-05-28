99a30b4948f860eac0059c8c3b664c02f8d19af4
Arturo <arthursolokhin27@gmail.com>
2025-05-29 02:11:05 +0300

    added params validating

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/NewMatchServlet.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/NewMatchServlet.java
@@ -1,6 +1,5 @@
 package com.github.ar4ik4ik.tennisscoreboard.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ar4ik4ik.tennisscoreboard.model.dto.MatchRequestDto;
 import com.github.ar4ik4ik.tennisscoreboard.model.dto.PlayerRequestDto;
 import com.github.ar4ik4ik.tennisscoreboard.service.OngoingMatchesService;
@@ -8,13 +7,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
+@Log4j2
 @WebServlet(urlPatterns = "/new-match")
 public class NewMatchServlet extends HttpServlet {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
     private OngoingMatchesService ongoingMatchesService;
 
     @Override
@@ -23,13 +23,23 @@ public class NewMatchServlet extends HttpServlet {
                 .getAttribute("ongoingMatchesService");
     }
 
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        String firstPlayer = req.getParameter("firstPlayer");
-        String secondPlayer = req.getParameter("secondPlayer");
+        String firstPlayer = req.getParameter("firstPlayerName");
+        String secondPlayer = req.getParameter("secondPlayerName");
 
-        var match = ongoingMatchesService.createNewMatch(MatchRequestDto.builder()
+        log.debug("Creating new match with players {} and {}", firstPlayer, secondPlayer);
+
+        if (firstPlayer == null || secondPlayer == null || firstPlayer.isBlank() || secondPlayer.isBlank()
+                || firstPlayer.equals(secondPlayer)) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            req.setAttribute("error", "Имена игроков должны быть заполнены и различными");
+            return;
+        }
+
+        var matchUUID = ongoingMatchesService.createNewMatch(MatchRequestDto.builder()
                 .firstPlayer(PlayerRequestDto.builder()
                         .name(firstPlayer)
                         .build())
@@ -38,7 +48,6 @@ public class NewMatchServlet extends HttpServlet {
                         .build())
                 .build());
 
-        resp.getWriter().write(objectMapper.writeValueAsString(match));
-        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + matchUUID);
     }
 }

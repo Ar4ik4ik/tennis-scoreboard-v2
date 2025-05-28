7f9de02454f67699f10e9716899e9ffbc4a20244
Arturo <arthursolokhin27@gmail.com>
2025-05-29 02:11:50 +0300

    servlet improve

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/MatchScoreServlet.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/controller/MatchScoreServlet.java
@@ -1,25 +1,31 @@
 package com.github.ar4ik4ik.tennisscoreboard.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.ar4ik4ik.tennisscoreboard.exceptions.MatchPersistenceException;
+import com.github.ar4ik4ik.tennisscoreboard.model.State;
 import com.github.ar4ik4ik.tennisscoreboard.model.dto.ScoreIncreaseDto;
 import com.github.ar4ik4ik.tennisscoreboard.service.MatchScoreCalculationService;
+import com.github.ar4ik4ik.tennisscoreboard.service.OngoingMatchesService;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Objects;
 
-@WebServlet(urlPatterns = "/match-score/")
+@WebServlet(urlPatterns = "/match-score")
 public class MatchScoreServlet extends HttpServlet {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
     private MatchScoreCalculationService matchScoreCalculationService;
+    private OngoingMatchesService ongoingMatchesService;
 
     @Override
     public void init() {
         matchScoreCalculationService = (MatchScoreCalculationService) getServletContext()
                 .getAttribute("matchScoreService");
+        ongoingMatchesService = (OngoingMatchesService) getServletContext()
+                .getAttribute("ongoingMatchesService");
     }
 
     @Override
@@ -27,12 +33,32 @@ public class MatchScoreServlet extends HttpServlet {
         String matchId = req.getParameter("uuid");
         String playerId = req.getParameter("playerId");
 
-        var res = matchScoreCalculationService.addPointToPlayer(new ScoreIncreaseDto(
-                matchId,
-                Integer.parseInt(playerId)
-        ));
+        try {
+            var updatedMatch = matchScoreCalculationService.addPointToPlayer(new ScoreIncreaseDto(
+                    matchId,
+                    Integer.parseInt(playerId)
+            ));
 
-        resp.getWriter().write(objectMapper.writeValueAsString(res));
-        resp.setStatus(HttpServletResponse.SC_OK);
+            if (Objects.equals(updatedMatch.matchState().name(), State.FINISHED.name())) {
+                req.setAttribute("matchDto", updatedMatch);
+                req.getRequestDispatcher("/WEB-INF/views/finished-page.jsp").forward(req, resp);
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + matchId);
+            }
+        } catch (MatchPersistenceException e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        var uuid = req.getParameter("uuid");
+        var match = ongoingMatchesService.getOngoingMatch(uuid);
+
+        req.setAttribute("matchDto", match);
+        req.getRequestDispatcher("/WEB-INF/views/match-score.jsp").forward(req, resp);
     }
 }

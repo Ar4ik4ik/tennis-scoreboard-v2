d98cc52c4b65231b591d6f3b8a75aa2df7b47dd8
Arturo <arthursolokhin27@gmail.com>
2025-06-09 00:50:18 +0300

    fixed N+1 problem

--- src/main/java/com/github/ar4ik4ik/tennisscoreboard/persistence/repository/MatchRepository.java
+++ src/main/java/com/github/ar4ik4ik/tennisscoreboard/persistence/repository/MatchRepository.java
@@ -7,7 +7,7 @@ import org.hibernate.Session;
 import java.util.List;
 
 
-public class MatchRepository extends BaseRepository<Integer, MatchEntity> implements MatchRepositoryImpl{
+public class MatchRepository extends BaseRepository<Integer, MatchEntity> implements MatchRepositoryImpl {
 
     public MatchRepository() {
         super(MatchEntity.class);
@@ -15,8 +15,11 @@ public class MatchRepository extends BaseRepository<Integer, MatchEntity> implem
 
     public List<MatchEntity> findAll(int offset, int limit) {
         Session session = SessionManager.getSession();
-        return session.createQuery("SELECT m " + "FROM MatchEntity m" +
-                        " order by id desc", clazz)
+        return session.createQuery("SELECT m " + "FROM MatchEntity m " +
+                                "JOIN FETCH m.firstPlayer fp " +
+                                "JOIN FETCH m.secondPlayer sp " +
+                                "JOIN FETCH m.winner w" +
+                        " order by m.id desc", clazz)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
@@ -24,10 +27,13 @@ public class MatchRepository extends BaseRepository<Integer, MatchEntity> implem
 
     public List<MatchEntity> findAllByPlayerName(String playerName, int offset, int limit) {
         Session session = SessionManager.getSession();
-        return session.createQuery("SELECT m " + "FROM   MatchEntity m "
-                + "WHERE lower(m.firstPlayer.name) LIKE :name "
-                + "OR lower(m.secondPlayer.name) LIKE :name "
-                + "order by id desc", MatchEntity.class)
+        return session.createQuery("SELECT m " + "FROM MatchEntity m " +
+                        "JOIN FETCH m.firstPlayer fp " +
+                        "JOIN FETCH m.secondPlayer sp " +
+                        "JOIN FETCH m.winner w "
+                        + "where lower(fp.name) LIKE :name "
+                        + "OR lower(sp.name) LIKE :name "
+                        + "order by m.id desc", MatchEntity.class)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .setParameter("name", "%" + playerName.toLowerCase() + "%").getResultList();
@@ -35,15 +41,16 @@ public class MatchRepository extends BaseRepository<Integer, MatchEntity> implem
 
     public long getObjectCount() {
         Session session = SessionManager.getSession();
-        return (long) session.createQuery("SELECT count(m) from MatchEntity m").getSingleResult();
+        return session.createQuery("SELECT count(m) from MatchEntity m", Long.class)
+                .getSingleResult();
     }
 
     public long getObjectCount(String playerName) {
         Session session = SessionManager.getSession();
-        return (long) session.createQuery("SELECT count(m) " +
-                "from MatchEntity m " +
-                "where lower(m.firstPlayer.name) LIKE :name" +
-                " or (m.secondPlayer.name) LIKE :name")
+        return session.createQuery("SELECT count(m) " +
+                        "from MatchEntity m " +
+                        "where lower(m.firstPlayer.name) LIKE :name" +
+                        " or lower(m.secondPlayer.name) LIKE :name", Long.class)
                 .setParameter("name", "%" + playerName.toLowerCase() + "%").getSingleResult();
     }
 }
